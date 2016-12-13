4a5914f9ac3dc96fad6ffa00c7de7bb6e9180859
flwwsg <2319406132@qq.com>
2016-12-13 17:42:50 +0800

    12-13 multiprocessing

---
+++ system/signal1.py
@@ -0,0 +1,13 @@
+'''signal1.py'''
+import sys, signal, time
+
+def now():
+	return time.ctime(time.time())
+
+def onSignal(signum, stackframe):
+	print('Got signal', signum, 'at', now())
+
+signum = int(sys.argv[1])
+signal.signal(signum, onSignal)
+while True:
+	signal.pause()
---
+++ system/signal2.py
@@ -0,0 +1,14 @@
+'''signal2.py'''
+import sys, signal, time
+
+def now():
+	return time.ctime(time.time())
+
+def onSignal(signum, stackframe):
+	print('Got signal', signum, 'at', now())
+
+while True:
+	print('Setting at', now())
+	signal.signal(signal.SIGALRM, onSignal)
+	signal.alarm(5)
+	signal.pause()
