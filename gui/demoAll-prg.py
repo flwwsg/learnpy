1a72455ccdbc4a99740f9d07feeb6624a36ee425
flwwsg <2319406132@qq.com>
2016-12-14 13:53:43 +0800

    supporting win

---
+++ gui/demoAll-prg.py
@@ -0,0 +1,14 @@
+'''demoAll-prg.py'''
+
+from tkinter import *
+from launchmodes import PortableLauncher
+
+demoModules = ['demoDlg', 'demoRadio', 'demoCheck', 'demoScale']
+
+for demo in demoModules:
+	PortableLauncher(demo, demo+'.py')()
+
+root = Tk()
+root.title('Processes')
+Label(root, text='Multiple Toplevel window demo: command lines', bg='white').pack()
+root.mainloop()
